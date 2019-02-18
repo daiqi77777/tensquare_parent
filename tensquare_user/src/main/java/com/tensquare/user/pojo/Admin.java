f02e34789ca220b2afad4415a1e1cd8b15da42df
冯彧 <15600644443@163.com>
2019-02-18 22:14:45 +0800

    ADD
    User模块

---
+++ tensquare_user/src/main/java/com/tensquare/user/pojo/Admin.java
@@ -0,0 +1,33 @@
+package com.tensquare.user.pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+/**
+ * 实体类
+ * @author Administrator
+ *
+ */
+@Entity
+@Table(name="tb_admin")
+@Getter
+@Setter
+public class Admin implements Serializable{
+
+	@Id
+	private String id;//ID
+
+
+	
+	private String loginname;//登陆名称
+
+	private String password;//密码
+
+	private String state;//状态
+
+
+}
