76930a829cc23e19cebfccb9620950ba51b7c0b9
冯彧 <15600644443@163.com>
2019-02-20 18:55:30 +0800

    ADD
    jwt测试代码

---
+++ tensquare_common/src/test/java/com/tensquare/jwt/CreateJwt.java
@@ -0,0 +1,18 @@
+package com.tensquare.jwt;
+
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+
+public class CreateJwt {
+    public static void main(String[] args) {
+        JwtBuilder jwt = Jwts.builder()
+                .setId("666")
+                .setSubject("小马")
+                .setIssuedAt(new Date())
+                .signWith(SignatureAlgorithm.HS256,"moliya");
+        System.out.println(jwt.compact());
+    }
+}
